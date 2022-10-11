967b21629480b9a54577476ca8887afe9eb1cd47
amerwebwhales <amer.naser@webwhales.nl>
2022-10-11 11:22:16 +0200

    add multilingual

--- src/DlfHackaton2022ServiceProvider.php
+++ src/DlfHackaton2022ServiceProvider.php
@@ -2,12 +2,20 @@
 
 namespace WebWhales\DlfHackaton2022;
 
+use Illuminate\Database\Schema\Blueprint;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use WebWhales\DlfHackaton2022\Commands\DlfHackaton2022Command;
 
 class DlfHackaton2022ServiceProvider extends PackageServiceProvider
 {
+    public function boot()
+    {
+        Blueprint::macro('multilingual', function () {
+            $this->bigInteger('local_id');
+        });
+    }
+
     public function configurePackage(Package $package): void
     {
         /*
