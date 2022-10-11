488ecbc94b073fa2a92d39140b1d387e9a6e2816
basserd <basvd789@gmail.com>
2022-10-11 12:04:04 +0200

    Making a start to printing both the content direction and the href language tags.

--- src/DlfHackaton2022ServiceProvider.php
+++ src/DlfHackaton2022ServiceProvider.php
@@ -3,6 +3,7 @@
 namespace WebWhales\DlfHackaton2022;
 
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 use WebWhales\DlfHackaton2022\Commands\DlfHackaton2022Command;
@@ -13,6 +14,33 @@ class DlfHackaton2022ServiceProvider extends PackageServiceProvider
     {
         Blueprint::macro('multilingual', function () {
             $this->bigInteger('local_id');
+        });
+
+        Blade::directive('content_dir', function () {
+            // ToDo: Getting the content direction from the current selected language.
+            $contentDirection = '';
+
+            return 'dir="'.$contentDirection ?? 'ltr'.'"';
+        });
+
+        Blade::directive( 'href_language', function() {
+            // ToDo: Getting the current model post.
+            $post = '';
+
+            // ToDo: Getting the href languages for the current Model post.
+            // I assume this is a collection.
+            $postLanguages = collect();
+
+            // ToDo:
+            // - Looping over the translations of the current model.
+            // - Using a route helper to generate the URL's based on the Translations.
+            $postLanguages->each( function( $language ) use ( $post ) {
+
+                // Problems:
+                // - Routes defined by the developer will have different parameters.
+                // - ...
+                echo route( request()->route()->getName(), ['post' => $post, 'lang' => 'de']);
+            });
         });
     }
 
