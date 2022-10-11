e3956ef202d46f6e80826ab797741831365cf42e
basserd <basvd789@gmail.com>
2022-10-11 13:17:06 +0200

    Added a directive for adding the current language local within a "lang" tag.

--- src/DlfHackaton2022ServiceProvider.php
+++ src/DlfHackaton2022ServiceProvider.php
@@ -16,30 +16,35 @@ class DlfHackaton2022ServiceProvider extends PackageServiceProvider
             $this->bigInteger('local_id');
         });
 
-        Blade::directive('content_dir', function () {
+        Blade::directive('isContentLtrTag', function () {
             // ToDo: Getting the content direction from the current selected language.
-            $contentDirection = '';
+            $isRtl = '';
 
-            return 'dir="'.$contentDirection ?? 'ltr'.'"';
+            return 'dir="' . $isRtl ?? 'ltr' . '"';
         });
 
-        Blade::directive( 'href_language', function() {
+        Blade::directive('contentLangTag', function () {
+            return 'lang="' . str_replace('_', '-', app()->getLocale()) . '"';
+        });
+
+        Blade::directive('hrefLangTags', function () {
             // ToDo: Getting the current model post.
             $post = '';
 
             // ToDo: Getting the href languages for the current Model post.
             // I assume this is a collection.
-            $postLanguages = collect();
+            $postLocales = collect();
 
             // ToDo:
             // - Looping over the translations of the current model.
             // - Using a route helper to generate the URL's based on the Translations.
-            $postLanguages->each( function( $language ) use ( $post ) {
+            $postLocales->each(function ($language) use ($post) {
 
                 // Problems:
-                // - Routes defined by the developer will have different parameters.
+                // - Routes defined by the developer will have different parameters, I cannot simply call the route for the given Model.
+                // It should go through a middleware of some kind..
                 // - ...
-                echo route( request()->route()->getName(), ['post' => $post, 'lang' => 'de']);
+                echo route(request()->route()->getName(), ['post' => $post, 'locale' => 'de']);
             });
         });
     }
