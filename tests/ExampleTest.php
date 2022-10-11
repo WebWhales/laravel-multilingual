2e019227afe2223063fa233245bb43a704bd891f
redelschaap <redelschaap@users.noreply.github.com>
2022-10-11 10:23:36 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,12 +6,12 @@ it('can retrieve translations for a model', function () {
     /** @var TestModel $model */
     $model = (new TestModel([
         'locale_id' => 1,
-        'name' => 'Whale'
+        'name' => 'Whale',
     ]))->save();
 
     $translatedModel = (new TestModel([
         'locale_id' => 2,
-        'name' => 'Walvis'
+        'name' => 'Walvis',
     ]))->save();
 
     $translations = $model->translations()->get();
